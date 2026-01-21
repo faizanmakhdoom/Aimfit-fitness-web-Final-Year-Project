729327d18ad5557da91d76b33cbcb05e800503e9
SALIHA RIAZ <151842064+Salihaawan@users.noreply.github.com>
2026-01-21 16:52:47 +0500

    Move axios import to a new position

--- frontend/src/pages/BMICalculator.js
+++ frontend/src/pages/BMICalculator.js
@@ -1,5 +1,5 @@
-import axios from "axios";
 import React, { useState } from "react";
+import axios from "axios";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../BlogPost.css"; // Reuse your pink theme
