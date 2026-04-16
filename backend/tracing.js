4167a42f62dca283fcf2450ad220b38194edb7fd
faizan <faizan.makhdoom@zigron.com>
2026-04-16 10:41:15 +0500

    Refactor tracing setup for OTLP exporter

--- backend/tracing.js
+++ backend/tracing.js
@@ -2,18 +2,19 @@ const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-
-const traceExporter = new OTLPTraceExporter({
-  url: 'http://192.168.0.53:4318/v1/traces',
-});
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'aimfit-backend',
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://192.168.0.53:4318/v1/traces',
   }),
-  traceExporter,
+
+  resource: new Resource({
+    'service.name': 'aimfit-backend',   // 👈 SAFE STANDARD WAY
+  }),
+
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 sdk.start();
+
+console.log('Tracing initialized');
