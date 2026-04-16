e507d9bd16627b1c85d32a1f46ed4fa1a3576c40
faizan <faizan.makhdoom@zigron.com>
2026-04-16 10:45:34 +0500

    Update tracing.js

--- backend/tracing.js
+++ backend/tracing.js
@@ -1,15 +1,10 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { Resource } = require('@opentelemetry/resources');
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
     url: 'http://192.168.0.53:4318/v1/traces',
-  }),
-
-  resource: new Resource({
-    'service.name': 'aimfit-backend',   // 👈 SAFE STANDARD WAY
   }),
 
   instrumentations: [getNodeAutoInstrumentations()],
